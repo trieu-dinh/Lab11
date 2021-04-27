8de1a559f2a2f81d6e3559acac1db18c02e42663
Trieu-Dinh <trieu.dinh2014@yahoo.com>
2021-04-27 18:22:15 -0500

    Fixed spelling

--- fiboRecursive.m
+++ fiboRecursive.m
@@ -6,6 +6,6 @@ function fibonacciNumber = fiboRecursive(n)
         fibonacciNumber = 1;
     else
         count = count +1;
-        fibonacciNumber = fiboRecurisve(n-2) + fiboRecursive(n-1);
+        fibonacciNumber = fiboRecursive(n-2) + fiboRecursive(n-1);
     end
 end
